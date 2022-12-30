4731d1f022e0831bd652e1b493d477aada2aff3e
Irvan Alfaridzi <irvanalfaridzi@gmail.com>
2022-12-30 17:28:34 +0700

    remove the key

--- lib/utils/environment.dart
+++ lib/utils/environment.dart
@@ -7,16 +7,10 @@ abstract class Environment {
   // TODO: Do an investigation on why .env can't be accessed in production deployment live
   // Failed to load resource: the server responded with a status of 404 ()
 
-  static String get apiUrl =>
-      "https://api.notion.com/v1/"; //dotenv.env['API_URL'] ?? 'null';
-  static String get apiKey =>
-      "secret_khejJceAPQgo3QfvtMvS97YPQVHovBo0UOhFxZFk3BZ"; //dotenv.env['API_KEY'] ?? 'null';
-  static String get notionVersion =>
-      "2022-02-22"; //dotenv.env['NOTION_VERSION'] ?? 'null';
-  static String get techToolsKey =>
-      "3dbb41e448cc468ca7462e4e9a4bbf53"; //dotenv.env['TECHTOOLS_KEY'] ?? 'null';
-  static String get experienceKey =>
-      "f9e16463bba442569a728613cb9fc1fb"; //dotenv.env['EXPERIENCE_KEY'] ?? 'null';
-  static bool get showLog => false; //isTrue(dotenv.env['SHOW_LOG'] ?? 'false');
-
+  static String get apiUrl => dotenv.env['API_URL'] ?? 'null';
+  static String get apiKey => dotenv.env['API_KEY'] ?? 'null';
+  static String get notionVersion => dotenv.env['NOTION_VERSION'] ?? 'null';
+  static String get techToolsKey => dotenv.env['TECHTOOLS_KEY'] ?? 'null';
+  static String get experienceKey => dotenv.env['EXPERIENCE_KEY'] ?? 'null';
+  static bool get showLog => isTrue(dotenv.env['SHOW_LOG'] ?? 'false');
 }
